3b3441121d962e0b32675f26efc8d4d06ef3bf7d
Charles Cary <cclego@gmail.com>
2011-08-03 18:28:15 -0400

    building view profile box

--- mosaic.js
+++ mosaic.js
@@ -15,7 +15,7 @@ $(document).bind("FBLoaded", function() {
     $('#infoHideDivButton').fadeTo(1,0.5);
     $('#infoHideDivButton').mouseover(function(){$('#infoContent').fadeTo(4,1);$('#infoHideDivButton').fadeTo(4,1);});
     $('#infoHideDivButton').mouseleave(function(){$('#infoContent').fadeTo(4,0.25);$('#infoHideDivButton').fadeTo(4,0.25);});
-    $('#infoHideDivButton').click(function(){alert('here');});
+    $('#infoHideDivButton').click(function(){$('#infobar').animate({marginBottom: 0},5000);});
 });
 $(document).bind("loadingFriends", function() {
     $('#loadText').text("Loading Friends");
