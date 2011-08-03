047bf6842ab016f00c0bc92b99efa750127b3233
Charles Cary <cclego@gmail.com>
2011-08-03 18:53:35 -0400

    building view profile box

--- mosaic.js
+++ mosaic.js
@@ -1,9 +1,5 @@
 /* Events which trigger page changes */
 $(document).bind("FBLoaded", function() {
-    $('#loginButton').hide();
-    $('#loadingSpinner').show();
-    $('#loadText').show();
-
     $('#constant').fadeTo(1,0.5);
     $('#constant').mouseover(function(){$('#constant').fadeTo(4,1);});
     $('#constant').mouseleave(function(){$('#constant').fadeTo(4,0.25);});
@@ -26,6 +22,9 @@ $(document).bind("FBLoaded", function() {
     ); 
 });
 $(document).bind("loadingFriends", function() {
+    $('#loginButton').hide();
+    $('#loadingSpinner').show();
+    $('#loadText').show();
     $('#loadText').text("Loading Friends");
 });
 $(document).bind("displayPhotos", function() {
