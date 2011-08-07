d5d49a0b9033313aadc4b978c1bb5d543900781f
Randall Hunt <randallhunt@gmail.com>
2011-08-07 13:48:32 -0400

    spacing changes, removing some: log statements, old comments, old checks

--- mosaic.js
+++ mosaic.js
@@ -3,23 +3,23 @@ $(document).bind("FBLoaded", function() {
     $('#constant').fadeTo(1,0.5);
     $('#constant').mouseover(function(){$('#constant').fadeTo(4,1);});
     $('#constant').mouseleave(function(){$('#constant').fadeTo(4,0.25);});
-    
+
     $('#infoHideDivRight').mouseover(function(e){e.stopPropagation();});
     $('#infoHideDivRight').mouseleave(function(){$('#infoContent').fadeTo(4,0.25);$('#infoHideDivButton').fadeTo(4,0.25);});
     $('#infoHideDivLeft').mouseover(function(e){e.stopPropagation();});
     $('#infoHideDivLeft').mouseleave(function(){$('#infoContent').fadeTo(4,0.25);$('#infoHideDivButton').fadeTo(4,0.25);});
-    
+
     $('#infoContent').fadeTo(4,0.5);
     $('#infoHideDivButton').fadeTo(4,0.5);
     $('#infobar').mouseover(function(){$('#infoContent').fadeTo(4,1);$('#infoHideDivButton').fadeTo(4,1);});
     $('#infobar').mouseleave(function(){$('#infoContent').fadeTo(4,0.25);$('#infoHideDivButton').fadeTo(4,0.25);});
-        
+
     $("#infoHideDivButton").toggle(
-		function(){
- 			$("#infobar").animate({marginTop: "-35px"}, 1000);
- 		},
         function(){
-			$("#infobar").animate({marginTop: "-225px"}, 1000);
+            $("#infobar").animate({marginTop: "-35px"}, 1000);
+        },
+        function(){
+            $("#infobar").animate({marginTop: "-225px"}, 1000);
         }
     );
 });
@@ -30,17 +30,27 @@ $(document).bind("loadingFriends", function() {
     $('#loadText').text("Loading Friends");
     $("#infobar").animate({marginTop: "-225px"}, 1000);
 });
+$(document).bind('loadingPhotos', function() {
+    $('#loadText').text('Loading Photos');
+})
 $(document).bind("displayPhotos", function() {
     $('#loadingSpinner').hide();
     $('#loadText').hide();
     $("#photos").show();
 })
-
+$(document).bind('photoListItemClick', function() {
+    /* Query to get photos of you with clicked on user */
+    FB.api({
+        method: 'fql.query',
+        query: 'SELECT src_big FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject='+uid+') AND pid IN (SELECT pid FROM photo_tag WHERE subject='+$(this).attr('id')+')'
+    }, function(response) {
+        console.log(response);
+    })
+});
 /* * * * * * * * * * * * * * * * * * * *
  *     Actual mosaic shit goes here    *
  * * * * * * * * * * * * * * * * * * * */
 /* Create the namespace */
-/* I wanted public and private methods + fields.  Module pattern now.*/
 var Mosaic = Mosaic || new function(){
     var loadedUsers = new Array(); // this will store data about already loaded users to prevent duplicate calls
     this.buildMosaic = function(response) {
@@ -64,33 +74,18 @@ var Mosaic = Mosaic || new function(){
                 response = Mosaic.sortFriends(response);
                 /* Build the Mosaic */
                 photo_array = [];
+                imgObj = new Image(); //Image Object for preloading photos
                 photo_array.push('<ul id="photoList">');
                 $.each(response, function(index, friend) {
+                    imgObj.src = friend.pic_big;
                     photo_array.push('<li id="'+friend.uid+'" title="'+friend.name+'" class="something"><img src="'+friend.pic_big+'" /></li>');
                 });
                 photo_array.push('</ul>');
                 $("#photos").html(photo_array.join(''));
-                /* Mouse Event Handlers for Photos */
-loadedUsers
-                /* Setup the hover */
-                $("#photoList>li").hover(
-                    function() {
-                        //Show most recent interaction
-                    },
-                    function() {
-                        //Hide most recent interaction
-                    }
-                );
                 /* Setup the click */
-                $("#photoList>li").click(
+                $('#photoList>li').click(
                     function() {
-                        /* Query to get photos of you with clicked on user */
-                        FB.api({
-                            method: 'fql.query',
-                            query: 'SELECT src_big FROM photo WHERE pid IN (SELECT pid FROM photo_tag WHERE subject='+uid+') AND pid IN (SELECT pid FROM photo_tag WHERE subject='+$(this).attr('id')+')'
-                        }, function(response) {
-                            console.log(response);
-                        })
+                        $.event.trigger('photoListItemClick');
                     }
                 );
                 /* Finally display the photo wall */
@@ -113,27 +108,28 @@ loadedUsers
         //$('#pEducation').html(response[0].);
         //$('#pWork').html(response[0].education);
         $('#pWebsite').html(response[0].website);
-    }
+    };
     this.loadProfile = function(id, callback) {
-        if (id+'' in loadedUsers) {
-            return loadedUsers[id+''];
+        if (id in loadedUsers) {
+            return loadedUsers[id];
         } else {
             FB.api({
-                    method: 'fql.query',
-                    query: 'SELECT uid, name, pic_small, religion, birthday, sex, meeting_for, meeting_sex, relationship_status, significant_other_id, political, current_location, interests, music, tv, movies, books, quotes, about_me, hs_info, education_history, status, website FROM user WHERE uid='+id 
-               }, function(response) {
-                    loadedUsers[id+''] = response;
-                    callback(response); 
-               });
+                method: 'fql.query',
+                query: 'SELECT uid, name, pic_small, religion, birthday, sex, meeting_for, meeting_sex, relationship_status, significant_other_id, political, current_location, interests, music, tv, movies, books, quotes, about_me, hs_info, education_history, status, website FROM user WHERE uid='+id
+            }, function(response) {
+                loadedUsers[id] = response;
+                Mosaic.displayProfile(response);
+            });
         }
-    }; 
+    };
     this.sortFriends = function(friends) {
         return friends;
-    },
+    };
     this.filterFriends = function(friends) {
         return friends;
-    }
+    };
 }();
+/*** END MOSAIC CODE ***/
 /* Load FB-async */
 window.fbAsyncInit = function() {
     FB.init({appId: '153403654734305', status: true, cookie: true,
